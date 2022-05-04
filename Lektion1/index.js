60734cf8af8e7c9de9cf882555e538ad494210d9
Gusben95 <bengtssongustav95@gmail.com>
2022-05-04 15:03:40 +0200

    Lägg till
    Ta bort

--- Lektion1/index.js
+++ Lektion1/index.js
@@ -3,15 +3,32 @@
 //localStorage.getItem('name');
 // Hämtar med hjälp av (nyckel). 
 
-const todosElem = document.querySelector('#todos');
+const todosElem = document.querySelector('#todos')
 const todosFromStorage = localStorage.getItem('myTodos')
+
+
+
+
 function createTodoItem(todo){
     const todoElem = document.createElement('li');
+    todoElem.id = todo.id; 
     todoElem.innerHTML = todo.task; 
     todosElem.append(todoElem);
+    todoElem.addEventListener('click', function(){
     
-
+       editLocalStorage(todo.id)
+       this.remove();
+    
+       
+    })
 }
+function editLocalStorage(id){
+
+    let myTodos = JSON.parse(localStorage.getItem('myTodos'))
+         myTodos = myTodos.filter(todo => todo.id !=id)
+         localStorage.setItem('myTodos' , JSON.stringify(myTodos))
+} 
+
 
 function saveToLocalStorage(todos){
 localStorage.setItem('myTodos', JSON.stringify(todos));
